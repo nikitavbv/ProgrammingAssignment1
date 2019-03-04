0d508fe4204d250bf53db5990c56affadaff81a7
Brook333 <maher5ter270301@gmail.com>
2019-03-04 20:40:56 +0200

    fix(main): add character encoding

--- src/main/java/com/nikitavbv/labs/introtoprogramming/lab1/Main.java
+++ src/main/java/com/nikitavbv/labs/introtoprogramming/lab1/Main.java
@@ -2,8 +2,10 @@ package com.nikitavbv.labs.introtoprogramming.lab1;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,7 +24,7 @@ public class Main {
   }
 
   public static List<Student> loadStudentList(File file) throws IOException {
-    return new BufferedReader(new FileReader(file))
+    return new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8))
             .lines()
             .skip(1)
             .map(Main::parseStudent)
