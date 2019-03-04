ce2597a8ba241496797a281c57808171e5cf7ca9
Brook333 <maher5ter270301@gmail.com>
2019-03-04 13:51:30 +0200

    refactor(student): use List instead of ArrayList

--- src/main/java/com/nikitavbv/labs/introtoprogramming/lab1/Student.java
+++ src/main/java/com/nikitavbv/labs/introtoprogramming/lab1/Student.java
@@ -1,14 +1,14 @@
 package com.nikitavbv.labs.introtoprogramming.lab1;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
 public class Student {
   private String name;
-  private ArrayList<Integer> subjectsMarks;
+  private List<Integer> subjectsMarks;
   private boolean isContract;
 
-  Student(String name, ArrayList<Integer> marks, boolean isContract) {
+  Student(String name, List<Integer> marks, boolean isContract) {
     this.name = name;
     subjectsMarks = marks;
     this.isContract = isContract;
