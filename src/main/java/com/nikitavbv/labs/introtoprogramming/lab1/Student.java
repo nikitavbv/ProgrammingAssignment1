7075891e9af5cd0348ca173ceecbe5f567127954
Nikita Volobuev <nikitavbv@gmail.com>
2019-03-05 20:14:06 +0200

    style(student): add javadoc

--- src/main/java/com/nikitavbv/labs/introtoprogramming/lab1/Student.java
+++ src/main/java/com/nikitavbv/labs/introtoprogramming/lab1/Student.java
@@ -5,6 +5,12 @@ import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
+/**
+ * This contains information about specific student.
+ *
+ * @author Nikita Volobuev
+ * @author Bohdan Fedorchenko
+ */
 public class Student {
   String name;
   List<Integer> subjectsMarks;
@@ -16,6 +22,14 @@ public class Student {
     this.isContract = isContract;
   }
 
+  /**
+   * Parse student from csv line.
+   * Expected format is:
+   * name,subject scores (comma separated),is contract (TRUE or FALSE)
+   *
+   * @param line csv line to parse
+   * @return parsed student object
+   */
   static Student fromCsv(String line) {
     String[] info = line.split(",");
     String name = info[0];
@@ -26,6 +40,11 @@ public class Student {
     return new Student(name, marks, isContract);
   }
 
+  /**
+   * Returns average score across all subjects of this student.
+   *
+   * @return average score or 0, if student has no scores.
+   */
   public double getAverageScore() {
     return subjectsMarks.stream()
             .collect(Collectors.averagingDouble(el -> el));
@@ -35,6 +54,13 @@ public class Student {
     return isContract;
   }
 
+  /**
+   * Returns csv representation of student.
+   * The format is:
+   * name,average subject score (rounded up to 3 decimal places)
+   *
+   * @return csv line
+   */
   public String toCSV() {
     return String.format(Locale.ENGLISH, "%s,%.3f", name, getAverageScore());
   }
