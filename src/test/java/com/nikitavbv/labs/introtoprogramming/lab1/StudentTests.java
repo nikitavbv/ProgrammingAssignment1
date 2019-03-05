be8e4b5df1ee2678990c1dad3e4380845ff695bb
Brook333 <maher5ter270301@gmail.com>
2019-03-05 13:20:45 +0200

    test(student): add test for toString

--- src/test/java/com/nikitavbv/labs/introtoprogramming/lab1/StudentTests.java
+++ src/test/java/com/nikitavbv/labs/introtoprogramming/lab1/StudentTests.java
@@ -6,19 +6,45 @@ import static junit.framework.TestCase.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class StudentTests {
 
+  private static final List<Student> DUMMY_STUDENTS = Arrays.asList(
+          new Student("John Doe", Arrays.asList(70, 80, 90, 60, 65), false),
+          new Student("John Doe", Collections.emptyList(), true),
+          new Student("", Arrays.asList(10, 10, 10, 10, 10), false),
+          new Student("Hans Christian Andersen", Collections.singletonList(10), true)
+  );
+
   @Test
   public void testStudentToCSV() {
-    Student student = new Student("John Doe", Arrays.asList(70, 80, 90, 60, 65), false);
-    assertEquals("John Doe,73.000", student.toCSV());
+    assertEquals("John Doe,73.000", DUMMY_STUDENTS.get(0).toCSV());
   }
 
   @Test
   public void testStudentWithNoSubjectsToCSV() {
-    Student student = new Student("John Doe", Collections.emptyList(), false);
-    assertEquals("John Doe,0.000", student.toCSV());
+    assertEquals("John Doe,0.000", DUMMY_STUDENTS.get(1).toCSV());
+  }
+
+  @Test
+  public void testStudentToString() {
+    assertEquals("John Doe             73.000", DUMMY_STUDENTS.get(0).toString());
+  }
+
+  @Test
+  public void testStudentToStringWithNoMarks() {
+    assertEquals("John Doe             0.000", DUMMY_STUDENTS.get(1).toString());
+  }
+
+  @Test
+  public void testStudentToStringWithEmptyName() {
+    assertEquals("                     73.000", DUMMY_STUDENTS.get(2).toString());
+  }
+
+  @Test
+  public void testStudentToStringWithLongName() {
+    assertEquals("Hans Christian Andersen 73.000", DUMMY_STUDENTS.get(3).toString());
   }
 
 }
